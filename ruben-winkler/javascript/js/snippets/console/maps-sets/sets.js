565775c2de6568a683c60c585043f18bab9162b4
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-01-20 16:54:24 +0100

    Weiter, immer weiter...

--- ruben-winkler/javascript/js/snippets/console/maps-sets/sets.js
+++ ruben-winkler/javascript/js/snippets/console/maps-sets/sets.js
@@ -9,6 +9,10 @@ const myObj = {
     "Pustekuchen ist kein Kuchen auf den man pustet - Aber man kann auf Kuchen pusten.",
 };
 
+function myFunc() {
+  return 11 + 22;
+}
+
 let mySet = new Set();
 
 mySet.add(1 + 1 - 52 + 120);
@@ -16,5 +20,25 @@ mySet.add(myVar);
 mySet.add(myLet);
 mySet.add(myArr);
 mySet.add(myObj);
+mySet.add(myFunc());
 
+console.log(mySet);
+
+console.log("------");
+
+console.log(mySet.has("Hallo Welt"));
+console.log(mySet.has(11 + 22));
+console.log(mySet.has("22"));
+
+console.log("------");
+console.log(mySet.delete("Hallo Welt"));
+console.log(mySet.delete(myObj));
+console.log(mySet);
+
+console.log("------");
+
+console.log("------");
+console.log(mySet.size);
+
+mySet.clear();
 console.log(mySet);
