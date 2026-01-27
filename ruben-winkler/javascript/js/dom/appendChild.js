eeda3c8d23bc925e5225f5d0973f1ba1f528c2a0
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-01-27 18:09:57 +0100

    dsfd

--- ruben-winkler/javascript/js/dom/appendChild.js
+++ ruben-winkler/javascript/js/dom/appendChild.js
@@ -1,13 +1,31 @@
-let myArticle = document.createElement("article");
-let jumbotron = document.querySelector(".jumbotron > section");
+"use strict";
 
-myArticle.setAttribute("class", "blogArticle");
-myArticle.setAttribute("id", "1");
+const menuItems = [
+  { text: "Startseite", href: "index.html", active: true },
+  { text: "HTML", href: "html.html" },
+  { text: "CSS", href: "css.html" },
+  { text: "JavaScript", href: "js.html" },
+];
 
-// Text hinzufügen mit createTextNode
-myText = document.createTextNode(
-  "Lorem Ipsum dolor sit amet. Lorem Ipsum dolor sit amet. Lorem Ipsum dolor sit amet. Lorem Ipsum dolor sit amet.",
-);
+const myContent = document.getElementById("content");
 
-myArticle.appendChild(myText);
-jumbotron.append(myArticle);
+const mySection = document.createElement("div");
+const myUList = document.createElement("ul");
+
+menuItems.forEach((item) => {
+  const li = document.createElement("li");
+  const link = document.createElement("a");
+
+  link.textContent = item.text;
+  link.href = item.href;
+
+  if (item.active) {
+    li.classList.add("active");
+  }
+
+  li.appendChild(link);
+  myUList.appendChild(li);
+});
+
+mySection.appendChild(myUList);
+myContent.appendChild(mySection);
--- ruben-winkler/javascript/js/dom/id.js
+++ ruben-winkler/javascript/js/dom/id.js
@@ -1,5 +1,11 @@
 "use strict";
 
 let myNavigation = document.querySelector("#navigation");
+<<<<<<< Updated upstream
 myNavigation.id = "new-id";
 myNavigation.id = "";
+=======
+
+let myId = myNavigation.id;
+myId.id = "newID";
+>>>>>>> Stashed changes
